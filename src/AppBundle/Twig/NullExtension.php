f3c8eed3066395755f4021c1e9cc0d5b7a35d2d2
Paul Daniel Vidal <pauldaniel.vidal@insead.edu>
2016-12-05 11:34:20 +0800

    Default session cookie's name should be changed. Deprecated class found in service definition

---
+++ src/AppBundle/Twig/NullExtension.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: INSEAD
+ * Date: 4/12/16
+ * Time: 4:56 PM
+ */
+namespace AppBundle\Twig;
+
+class NullExtension implements \Twig_ExtensionInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function initRuntime(\Twig_Environment $environment)
+    {}
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTokenParsers()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getNodeVisitors()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFilters()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTests()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFunctions()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getOperators()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getGlobals()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'null_extension';
+    }
+}
